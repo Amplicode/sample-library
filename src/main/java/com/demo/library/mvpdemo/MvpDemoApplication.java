6d03a7275e9b761fde43f6ed69fd3f6aabdfdd1e
budarov <budarov@haulmont.com>
2021-09-28 12:59:05 +0400

    empty project created

---
+++ src/main/java/com/demo/library/mvpdemo/MvpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.library.mvpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvpDemoApplication.class, args);
+    }
+
+}
