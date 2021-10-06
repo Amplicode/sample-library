4bb3ff302394904d2d3c47c6959168b418bc2793
budarov <budarov@haulmont.com>
2021-10-06 18:47:26 +0400

    fixes for react buddy preview

--- frontend/src/dev/previews.tsx
+++ frontend/src/dev/previews.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { ComponentPreview, Previews } from "@react-buddy/ide-toolbox";
 import App from "../app/App";
+import BookList from "../app/books/BookList";
+import BookDetails from "../app/books/BookDetails";
+import AuthorList from "../app/authors/AuthorList";
 
 export const ComponentPreviews = () => {
     return (
         <Previews>
             <ComponentPreview path="/App">
                 <App />
+            </ComponentPreview>
+            <ComponentPreview path="/BookList">
+                <BookList/>
+            </ComponentPreview>
+            <ComponentPreview path="/BookDetails">
+                <BookDetails/>
+            </ComponentPreview>
+            <ComponentPreview path="/AuthorList">
+                <AuthorList/>
             </ComponentPreview>
         </Previews>
     );
